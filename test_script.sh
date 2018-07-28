68a7dc35c9fcd0baeae5ea27f897828c94eec1bc
Rahul <rahulm.mailtome@gmail.com>
2018-07-28 09:35:44 +0000

    modified script

--- test_script.sh
+++ test_script.sh
@@ -51,6 +51,7 @@ echo "The test script is in use...!!"
 echo "Waiting for user $4 to exit..(working since $7 $8 $9)...!!"
 echo "Try after some time..!!"
 exit 0
+fi
 touch $HOME/script.lock
 trap remove_lock 1 2 15
 echo ""
@@ -77,6 +78,6 @@ case $selection in
 4 ) print_hello ; rm -f $HOME/script.lock ; press_enter ;;
 0 ) rm -f $HOME/script.lock ; exit ;;
 * ) echo "Please enter 1 or 2 or 3 or 4 or 0"; rm -f $HOME/script.lock ; press_enter
+esac
 done
-
 exit 0
